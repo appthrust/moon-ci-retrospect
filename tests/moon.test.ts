f8915145e214bdf25834f9c957e8aaa2e3fe0920
0xar-ds <sadden-rope-sleek@duck.com>
2025-09-10 00:38:17 -0300

    feat: add @vercel/ncc to handle building & bundling
    chore: update tsconfig.json
    
    chore: update release destination
    
    chore: bump yarn lock

--- tests/moon.test.ts
+++ tests/moon.test.ts
@@ -2,18 +2,18 @@ import * as path from "node:path";
 import { $ } from "execa";
 import { expect, test } from "vitest";
 
-const indexJs = path.resolve("index.js");
+const indexJs = path.resolve("dist/index.js");
 
 test("basic", async () => {
-	const cwd = path.join(import.meta.dirname, "workspaces/basic");
-	const { stdout, stderr } = await $({ cwd })`node ${indexJs}`;
-	expect(stdout).toMatchSnapshot();
-	expect(stderr).toBe("");
+  const cwd = path.join(import.meta.dirname, "workspaces/basic");
+  const { stdout, stderr } = await $({ cwd })`node ${indexJs}`;
+  expect(stdout).toMatchSnapshot();
+  expect(stderr).toBe("");
 });
 
 test("no-tasks", async () => {
-	const cwd = path.join(import.meta.dirname, "workspaces/no-tasks");
-	const { stdout, stderr } = await $({ cwd })`node ${indexJs}`;
-	expect(stdout).toMatchSnapshot();
-	expect(stderr).toBe("");
+  const cwd = path.join(import.meta.dirname, "workspaces/no-tasks");
+  const { stdout, stderr } = await $({ cwd })`node ${indexJs}`;
+  expect(stdout).toMatchSnapshot();
+  expect(stderr).toBe("");
 });
